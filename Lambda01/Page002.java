365cd7dd9262f46746fba3a32c25bb9e72b580a9
next-fire-starter <next.fire.starter@gmail.com>
2021-05-20 15:09:03 +0000

    add extensions

--- Lambda01/Page002.java
+++ Lambda01/Page002.java
@@ -1,5 +1,8 @@
 public class Page002 {
 	public static void main(String[] args){	
-		System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
 	}
 }
